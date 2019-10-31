e10ca57fad2f754aace1bd4a10e130e379f61859
Elvis Metodiev <metodiev.design@gmail.com>
2019-11-01 00:08:31 +0200

    Updated version.

--- Pick a number less than 5/src/pickanumberlessthanfive.java
+++ Pick a number less than 5/src/pickanumberlessthanfive.java
@@ -5,40 +5,43 @@ public class pickanumberlessthanfive {
 	{
 		Scanner scan = new Scanner(System.in);
 
-		boolean run = true;
+		// start the program in a loop.
 		while(true) {
-		    //do whatever
 
+			// provide a basic CLI interface (prompt).
 			System.out.println("Pick a number less than 5");
 
+			// wait for an INT input.
 			int userinput = scan.nextInt();
 
+			// show the int that was picked.
 			System.out.println("You picked " + userinput);
 
+			// if int is less than 5 -> show the number that was entered by the user.
 			if (userinput < 5) {
 				System.out.println("Note: The number you picked, " + userinput + ", is less than 5! Great job!");
-
 			}
-			else
+			// else -> error out.
+			else {
 				System.out.println("ERROR! You should pick a number less than 5");
 
-		    System.out.println("Would you like to continue? (yes or no)");
+				// ask if they want to play again.
+				System.out.println("Would you like to continue? (yes or no)");
 
-		    Scanner scann = new Scanner(System.in);
+				Scanner scann = new Scanner(System.in);
 
-		    String s = scann.nextLine();
+				String s = scann.nextLine();
 
-		    if (s.equals("no")) {
-		        run = false;
-
-		       System.out.println("Thank you for playing this game!");
-		       break;
-
-		    }
-		    else {
-		    	run = true;
-		 		}
-
+				// if "no" -> exit.
+				if (s.equals("no")) {
+					System.out.println("Thank you for playing this game!");
+					break;
+				}
+				// else -> play again.
+				else {
+					continue;
+				}
+			}
 		}
 	}
 }
